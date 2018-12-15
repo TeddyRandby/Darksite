f2e601837aca8e4c62efe2f594ee6f4459a0b6ca
TeddyRandby <tedrandby@gmail.com>
2018-12-15 00:12:04 -0500

    pages maded

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,27 @@ const request = require('request');
 class IndexRoute extends route_1.BaseRoute {
     static create(router) {
         console.log("[IndexRoute::create] Creating index route.");
+
         router.get("/", (req, res, next) => {
             new IndexRoute().index(req, res, next);
         });
         router.post("/", (req, res, next) => {
             new IndexRoute().index(req, res, next);
+        });
+        router.get("/contact", (req, res, next) => {
+            new IndexRoute().contact(req, res, next);
+        });
+        router.get("/history", (req, res, next) => {
+            new IndexRoute().history(req, res, next);
+        });
+        router.get("/merch", (req, res, next) => {
+            new IndexRoute().merch(req, res, next);
+        });
+        router.get("/photos", (req, res, next) => {
+            new IndexRoute().photos(req, res, next);
+        });
+        router.get("/roster", (req, res, next) => {
+            new IndexRoute().roster(req, res, next);
         });
     }
 
@@ -26,6 +42,46 @@ class IndexRoute extends route_1.BaseRoute {
             "message": "Hello World"
         };
         this.render(req, res, "index", options);
+    }
+
+    contact(req, res, next) {
+        this.title = "Darkide";
+        let options = {
+            "message": "Hello World"
+        };
+        this.render(req, res, "contact", options);
+    }
+
+    history(req, res, next) {
+        this.title = "Darkide";
+        let options = {
+            "message": "Hello World"
+        };
+        this.render(req, res, "history", options);
+    }
+
+    merch(req, res, next) {
+        this.title = "Darkide";
+        let options = {
+            "message": "Hello World"
+        };
+        this.render(req, res, "merch", options);
+    }
+
+    photos(req, res, next) {
+        this.title = "Darkide";
+        let options = {
+            "message": "Hello World"
+        };
+        this.render(req, res, "photos", options);
+    }
+
+    roster(req, res, next) {
+        this.title = "Darkide";
+        let options = {
+            "message": "Hello World"
+        };
+        this.render(req, res, "roster", options);
     }
 
   }
